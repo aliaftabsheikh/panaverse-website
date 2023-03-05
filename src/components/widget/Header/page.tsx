85db4dfbdcaf251b8aff652c394a98bf5c511bc3
aliaftabsheikh <80570243+aliaftabsheikh@users.noreply.github.com>
2023-03-05 16:34:49 +0500

    fix: issues

--- src/components/widget/Header/page.tsx
+++ src/components/widget/Header/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -7,42 +7,47 @@ import React, { useState } from "react";
 import { images } from "../../../../public/constants";
 
 export default function Header() {
+  const [customNavbar, setCustomNavbar] = useState(false);
 
-  const [customNavbar, setCustomNavbar] = useState(false)
-
-  const changeBackground = ()=>{
-    
-    if(window.scrollY >= 80){
-      setCustomNavbar(true)
-    }else{
-      setCustomNavbar(false)
+  const changeBackground = () => {
+    if (typeof window !== undefined) {
+      if (window.scrollY >= 80) {
+        setCustomNavbar(true);
+      } else {
+        setCustomNavbar(false);
+      }
     }
-  }
+  };
+  window.addEventListener("scroll", changeBackground);
 
-  window.addEventListener('scroll', changeBackground)
   return (
-      <header className={`${customNavbar ? 'bg-white' : 'bg-transparent'} w-full flex justify-center ${customNavbar ? 'sticky' : 'relative'} top-0 transition-all ${customNavbar ? '' : 'bgtop-lines-s'} `}>
-        <section className="flex justify-between items-center w-[1200px]">
-          <Link href={'/'}>
+    <header
+      className={`${
+        customNavbar ? "bg-white" : "bg-transparent"
+      } w-full flex justify-center ${
+        customNavbar ? "sticky" : "relative"
+      } top-0 transition-all ${customNavbar ? "" : "bgtop-lines-s"} `}
+    >
+      <section className="flex justify-between items-center w-[1200px]">
+        <Link href={"/"}>
           <Image
             className="w-28 py-2"
-            src={customNavbar ? images.panaverseLogo : images.panaverseLogoWhite}
+            src={
+              customNavbar ? images.panaverseLogo : images.panaverseLogoWhite
+            }
             alt="panaverse-logo"
           />
-          </Link>
+        </Link>
 
-          <ul className={`flex ${customNavbar ? 'text-black' : 'text-white'}  space-x-5 cursor-pointer font-semibold`}>
-            <li>
-              Home
-            </li>
-            <li>
-              Courses
-            </li>
-          </ul>
-        </section>
-      </header>
-
-    
-
+        <ul
+          className={`flex ${
+            customNavbar ? "text-black" : "text-white"
+          }  space-x-5 cursor-pointer font-semibold`}
+        >
+          <li>Home</li>
+          <li>Courses</li>
+        </ul>
+      </section>
+    </header>
   );
 }
