bcd5be906890c5670058c27fcffa9c6b28a02491
aliaftabsheikh <80570243+aliaftabsheikh@users.noreply.github.com>
2023-03-21 04:56:12 +0500

    fix:issue

--- src/pages/api/tracks/[track].ts
+++ src/pages/api/tracks/[track].ts
@@ -3,6 +3,6 @@ import { specializedTracks } from '@/config';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const data = specializedTracks.find((item) => item.id === req.query.track)
-    const filteredData = data?.quarters.find((item) => item.id === Number(req.query.quarter))
+    const filteredData = data?.quarter.find((item) => item.id === Number(req.query.quarter))
     res.status(200).json({ trackName: data?.name, data: filteredData });
 }
